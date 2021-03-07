b74c354610cdeba37d279bba2a9edcbf555e44b6
ikuchin <ivan.kuchin@gmail.com>
2021-03-06 21:50:29 -0500

    development in-progress

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#if [[ -z "$GITHUB_TOKEN" ]]; then
-#	echo "The GITHUB_TOKEN is required."
-#	exit 1
-#fi
+if [[ -z "$INPUT_GITHUB_TOKEN" ]]; then
+	echo "The GITHUB_TOKEN is required."
+	exit 1
+fi
 
 env
 
